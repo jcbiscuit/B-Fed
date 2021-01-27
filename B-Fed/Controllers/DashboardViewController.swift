9b4bc9eb74a7406c96c663f0cf60f580047bb1c6
Jody Mazinyi <jodyclelland8@gmail.com>
2021-01-28 10:55:39 +1100

    changes to VCs

---
+++ B-Fed/Controllers/DashboardViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DashboardViewController.swift
+//  B-Fed
+//
+//  Created by Jody Clelland on 27/1/21.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
