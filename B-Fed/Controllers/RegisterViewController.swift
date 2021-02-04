957255d0d6b0662128bc9a284f6a263221ab7deb
Jody Mazinyi <jodyclelland8@gmail.com>
2021-02-04 23:29:03 +1100

    changes to UI

--- B-Fed/Controllers/RegisterViewController.swift
+++ B-Fed/Controllers/RegisterViewController.swift
@@ -10,24 +10,35 @@ import Firebase
 
 class RegisterViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var registerLabel: UILabel!
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailAddressTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordError: UILabel!
     
     
     @IBAction func registerButtonPressed(_ sender: UIButton) {
-    if let email = emailAddressTextField.text, let password = passwordTextField.text {
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+        if let email = emailAddressTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             if error != nil {
                 self.passwordError.text = error!.localizedDescription
                 self.passwordError.alpha = 1
             }  else {
                 self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                
             }
+              
         }
     }
     }
+//    let db = Firestore.firestore()
+//
+//    db.collection("users").addDocument(data: ["first name": self.firstNameTextField, "last name": self.lastNameTextField, "uid":authResult!.user.uid]) { (error) in
+//
+//        if error != nil {
+//            self.passwordError.text = error!.localizedDescription
+//        }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
